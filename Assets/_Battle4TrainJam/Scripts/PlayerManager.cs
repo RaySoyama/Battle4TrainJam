a6126a27c133039ce43960f7a2f7eeadf4f3963d
Ray Soyama <raysoyama@gmail.com>
2019-10-16 22:49:58 -0700

    Audio System WIP

--- Assets/_Battle4TrainJam/Scripts/PlayerManager.cs
+++ Assets/_Battle4TrainJam/Scripts/PlayerManager.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private Animator anim;
+
+
+    [Header("Stats")]
+    [SerializeField]
+    private float speed = 3;
+    [SerializeField]
+    private float combatRange = 9;
+
+
+
+    [Header("Inventory Stuff")]
+
     [SerializeField]
     private int inventoryMaxCap = 25;
-
-
 
     public List<ItemSO> inventory;
 
     public int inventorySize;
 
+    [SerializeField]
+    private GameObject backpackInCombat;
 
+    [SerializeField]
+    private GameObject backpackNonCombat;
+
+    [SerializeField]
+    private float backpackToggleSpeed = 0.4f;
 
 
     void Start()
@@ -23,11 +43,58 @@ public class PlayerManager : MonoBehaviour
 
     void Update()
     {
-        
+        if (WorldMachine.World.currentState == WorldMachine.State.Walking)
+        {
+            WalkingUpdate();
+        }
+        else
+        {
+            backpackNonCombat.SetActive(false);
+            backpackInCombat.SetActive(true);
+        }
+
+
+        if (Input.GetKey(KeyCode.T))
+        {
+            backpackInCombat.transform.localScale =  Vector3.Lerp(backpackInCombat.transform.localScale, Vector3.one, backpackToggleSpeed * Time.deltaTime);
+            backpackNonCombat.transform.localScale =  Vector3.Lerp(backpackNonCombat.transform.localScale, Vector3.one, backpackToggleSpeed * Time.deltaTime);
+        }
+        else
+        {
+            backpackInCombat.transform.localScale = Vector3.Lerp(backpackInCombat.transform.localScale, Vector3.zero, backpackToggleSpeed * Time.deltaTime);
+            backpackNonCombat.transform.localScale = Vector3.Lerp(backpackNonCombat.transform.localScale, Vector3.zero, backpackToggleSpeed * Time.deltaTime);
+        }
+
     }
 
 
+    private void WalkingUpdate()
+    {
+        CheckRangeOfEnemies();
+
+        backpackNonCombat.SetActive(true);
+        backpackInCombat.SetActive(false);
+
+        //play animation of walking
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+
+    private void CheckRangeOfEnemies()
+    {
+        foreach (EnemyController EC in WorldMachine.World.AllEnemies)
+        {
+            if (EC.transform.position.x - transform.position.x < combatRange)
+            {
+                WorldMachine.World.enemyInCombat = EC;
+                WorldMachine.World.currentState = WorldMachine.State.EnterCombat;
+                //turn off animation for walking
+                return;
+            }
+        }
     
+    }
+
     public bool AddItemToBag(ItemSO newItem)
     {
         if(inventorySize + newItem.Size > 25)
@@ -48,7 +115,5 @@ public class PlayerManager : MonoBehaviour
         inventorySize -= newItem.Size;    
     }
 
-
-
-
+    
 }
--- Assets/_Battle4TrainJam/Scripts/WorldMachine.cs
+++ Assets/_Battle4TrainJam/Scripts/WorldMachine.cs
@@ -1,13 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Cinemachine;
 
 public class WorldMachine : MonoBehaviour
 {
+    public enum State
+    { 
+        None,
+        Walking,
+        EnterCombat,
+        PreAction,
+        Action,
+        Win,
+        Death
+    }
+
+
     public static WorldMachine World;
+
 
     [SerializeField]
     private AudioSource HeatbeatAudioSource = null;
+
+    [SerializeField]
+    private AudioSource BassAudioSource = null;
+
+    [SerializeField]
+    private AudioSource UkuleleAudioSource = null;
+
+    [SerializeField]
+    private AudioSource SynthAudioSource = null;
+
+    [SerializeField]
+    private AudioSource ActionAudioSource = null;
+
+    [Space(10)]
+    
+
+    [SerializeField]
+    private CinemachineVirtualCamera walkingCam;
+
+    [SerializeField]
+    private CinemachineVirtualCamera combatCam;
+
+    [Space(10)]
 
     [SerializeField]
     private float BeatsPerMinute = 120;
@@ -18,10 +55,21 @@ public class WorldMachine : MonoBehaviour
     public int currentBeatIndex;
 
 
+    [Space(10)]
+    [Header("Enemy Stuff")]
+
+    [ReadOnlyField]
+    public EnemyController enemyInCombat;
+
+    [ReadOnlyField]
+    public State currentState;
+
+    public List<EnemyController> AllEnemies;
+
     //player ref
     //enemy ref
 
-
+    
 
     private float beatDuration;
     void Start()
@@ -40,10 +88,32 @@ public class WorldMachine : MonoBehaviour
     {
 
         HeartBeatUpdate();
+        MusicUpdate();
 
 
-        //if in combat check
-        CombatUpdate();
+        if (currentState == State.Walking)
+        {
+            walkingCam.Priority = 11;
+            combatCam.Priority = 10;
+
+
+        }
+        else
+        {         
+            walkingCam.Priority = 10;
+            combatCam.Priority = 11;
+
+        }
+
+
+        if(Input.GetKeyDown(KeyCode.U))
+        {
+            currentState = State.Walking;
+        }
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            currentState = State.PreAction;
+        }
 
 
     }
@@ -57,20 +127,37 @@ public class WorldMachine : MonoBehaviour
             Debug.Log("BEAT");
 
             currentBeatIndex++;
-            HeatbeatAudioSource.Play();
+            //HeatbeatAudioSource.Play();
 
-            if (currentBeatIndex > 4)
+            if (currentBeatIndex > 8)
             {
                 currentBeatIndex = 1;
             }
         }
     }
 
-    private void CombatUpdate()
+    private void MusicUpdate()
     {
+        switch (currentState)
+        {
+            case State.Walking:
+                if (currentBeatIndex == 1 && UkuleleAudioSource.isPlaying == false)
+                {
+                    UkuleleAudioSource.Play();
+                }
 
+                break;
+            case State.PreAction:
+                if (currentBeatIndex == 1 && (UkuleleAudioSource.isPlaying == false || BassAudioSource.isPlaying == false))
+                {
+                    UkuleleAudioSource.Play();
+                    BassAudioSource.Play();
+                }
+                break;
 
+        }
 
     }
+
 
 }
